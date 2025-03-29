9a245298152f1c005f9a5bbf949e0e3d24b89780
Oleksii-Leo <al.provita@gmail.com>
2025-03-29 22:18:03 +0200

    task1

--- jsBasics/task1.js
+++ jsBasics/task1.js
@@ -1,1 +1,13 @@
-Test text
+// Оголошення змінних різних примітивних типів даних
+let str = "Привіт, світ!"; // Рядок
+let num = 42; // Число
+let bool = true; // Булеве значення
+let n = null; // Null
+let u; // Undefined
+
+// >> консоль
+console.log("Рядок:", str);
+console.log("Число:", num);
+console.log("Булеве значення:", bool);
+console.log("Null:", n);
+console.log("Undefined:", u);
