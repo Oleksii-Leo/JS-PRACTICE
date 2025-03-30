d56e98e4363b4805784d7b10a3f7de675e242e43
Oleksii-Leo <al.provita@gmail.com>
2025-03-30 18:44:49 +0300

    Changed > Hello my...

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -1,6 +1,6 @@
 const firstName = "Oleksii";
 const lastName = "Leonov";
 const fullName = firstName + " " + lastName; // Конкатенація
-let fullNameTemplate = `${firstName} ${lastName}`;
+let fullNameTemplate = `Hello my name is ${firstName} ${lastName}`;
 console.log(fullName);
 console.log(fullNameTemplate);
