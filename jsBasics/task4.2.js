b3adc9d14a8dc215bfe14904189921bd83d11351
Oleksii-Leo <al.provita@gmail.com>
2025-03-30 20:20:41 +0300

    4.2>

---
+++ jsBasics/task4.2.js
@@ -0,0 +1,9 @@
+// Змінні для довжини та ширини прямокутника
+const length = 10;
+const width = 5;
+//Формула//
+const area = length * width;
+// Виведення результату
+console.log("Довжина прямокутника:", length);
+console.log("Ширина прямокутника:", width);
+console.log("Площа прямокутника:", area);
