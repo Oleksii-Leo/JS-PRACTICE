53059c0c94bf3e177d89b8bac1e09db0b2ae00d0
Oleksii-Leo <al.provita@gmail.com>
2025-04-23 16:41:36 +0300

    9-1,2,3

---
+++ jsBasics/lesson9/task1.js
@@ -0,0 +1,11 @@
+// Створення порожнього об'єкта
+let student = {};
+
+// Додавання властивостей
+student.firstName = "Олександр";
+student.lastName = "Іваненко";
+student.age = 20;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+// Виведення об'єкта в консоль
+console.log(student);
